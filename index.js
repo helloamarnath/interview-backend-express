7cd73b868826c2d4af4e57bec4fa42fe8a1be23a
Amarnath TSR <tsr.amarnath@gmail.com>
2023-07-25 19:47:34 +0530

    body parser implement

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 const app = express();
 app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 app.listen(3000, () => {
     console.log('server established in port 3000');
 });
